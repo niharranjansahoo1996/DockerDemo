943b7e4726a406c45cc0869c998d379342aeec71
TL <TL@gmail.com>
2021-07-29 12:54:58 -0500

    Third commit

--- SpringBootDocker/src/main/java/com/nihar/controller/Controller.java
+++ SpringBootDocker/src/main/java/com/nihar/controller/Controller.java
@@ -1,10 +1,12 @@
 package com.nihar.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+	@GetMapping("/msg")
 	public String getmsg() {
 		return "Welcome.";
 	}
